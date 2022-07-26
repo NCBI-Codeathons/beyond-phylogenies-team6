bef97f17712418def5949fbf026bf08188369882
Chaoran Chen <mail@chaoran-chen.de>
2022-07-26 14:51:01 +0200

    build(api): fix Docker image and make gunicorn actually work

--- phylo-api/main.py
+++ phylo-api/main.py
@@ -39,30 +39,37 @@ app = Flask(__name__)
 app.register_blueprint(tree_api)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # If the program is directly called from the CLI
     # Parse arguments
     parser = argparse.ArgumentParser(description='Query and calculate statistics from a phylogenetic tree')
     parser.add_argument('datadir', type=str, help='path to the data directory')
     args = parser.parse_args()
-    print("Data directory: %s" % args.datadir)
+    datadir = args.datadir
+else:  # Defaults for the Docker container
+    datadir = '/data'
 
-    # Load data
-    if os.path.exists(os.path.join(args.datadir, 'public-latest.all.p')):
-        print("Loading tree from pickle file.")
-        tree = pickle.load(open(os.path.join(args.datadir, 'public-latest.all.p'), "rb"))
-    else:
-        print("Loading tree from newick file.")
-        tree = read_tree(os.path.join(args.datadir, 'public-latest.all.nwk'))
-        print("Saving tree as pickle file for faster loading next time.")
-        pickle.dump(tree, open(os.path.join(args.datadir, 'public-latest.all.p'), "wb" ))
-    tree = enhance_swift_tree(tree)
-    print("Loading metadata.")
-    metadata = read_metadata(os.path.join(args.datadir, 'basic_metadata.tsv'))
+print("Data directory: %s" % datadir)
 
-    # Pass data to API
-    api.tree = tree
-    api.metadata = metadata
 
+# Load data
+if os.path.exists(os.path.join(datadir, 'public-latest.all.p')):
+    print("Loading tree from pickle file.")
+    tree = pickle.load(open(os.path.join(datadir, 'public-latest.all.p'), "rb"))
+else:
+    print("Loading tree from newick file.")
+    tree = read_tree(os.path.join(datadir, 'public-latest.all.nwk'))
+    print("Saving tree as pickle file for faster loading next time.")
+    pickle.dump(tree, open(os.path.join(datadir, 'public-latest.all.p'), "wb" ))
+tree = enhance_swift_tree(tree)
+print("Loading metadata.")
+metadata = read_metadata(os.path.join(datadir, 'basic_metadata.tsv'))
+
+# Pass data to API
+api.tree = tree
+api.metadata = metadata
+
+print("Starting app.")
+
+if __name__ == '__main__':
     # Start API
-    print("Starting app.")
     app.run(host='0.0.0.0', port=2507)
